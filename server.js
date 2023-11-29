4e134e024b61377a5db31bc3819a315924d865cb
SigneAnnaMaria <signe.annamaria@gmail.com>
2023-11-29 21:51:56 +0100

    implement order validation and storage

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const bcrypt = require('bcrypt')
 const fs = require('fs')
 const usersFile = 'users.json'
+const ordersFile = 'orders.json'
+
+if (!fs.existsSync(usersFile)) {
+  fs.writeFileSync(usersFile, JSON.stringify({}))
+}
+
+if (!fs.existsSync(ordersFile)) {
+  fs.writeFileSync(ordersFile, JSON.stringify({}))
+}
 
 app.use(express.json())
 app.use(cors())
@@ -28,7 +37,12 @@ app.post('/register', async (req, res) => {
 
     const users = JSON.parse(fs.readFileSync(usersFile, 'utf-8'))
     users[email] = { email, password: hashedPassword }
-    fs.writeFileSync(usersFile, JSON.stringify(users))
+    try {
+      fs.writeFileSync(usersFile, JSON.stringify(users))
+      console.log('Data written to users.json successfully')
+    } catch (error) {
+      console.error('Error writing to users.json:', error)
+    }
 
     res.status(201).send('User registered successfully')
   } catch (error) {
@@ -40,6 +54,7 @@ app.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body
     const users = JSON.parse(fs.readFileSync(usersFile, 'utf-8'))
+    console.log('users', users)
 
     if (
       !users[email] ||
@@ -87,4 +102,76 @@ app.post('/cart/remove', (req, res) => {
     )
   }
   res.status(200).send('Product removed from cart')
+})
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    console.log('place order req', req.body)
+    const { items } = req.body 
+
+    const lineItems = items.map((item) => {
+      return {
+        price: item.priceId,
+        quantity: item.quantity,
+      }
+    })
+    console.log('place order 2')
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `${req.headers.origin}/checkout-success`, 
+      cancel_url: `${req.headers.origin}/checkout-failed`, 
+    })
+
+    res.status(200).json({ sessionId: session.id })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+app.post('/confirm-order', async (req, res) => {
+  try {
+    const orderDetails = req.body
+    const user = orderDetails.user
+    console.log('orderDetails', orderDetails)
+    console.log('user', user)
+    let orders = {}
+    if (fs.existsSync(ordersFile)) {
+      orders = JSON.parse(fs.readFileSync(ordersFile, 'utf-8'))
+    }
+
+    if (!orders[user]) {
+      orders[user] = []
+    }
+    console.log('before adding order orders', orders)
+
+    const now = new Date()
+    const formattedDate = `${now.toISOString().slice(0, 19).replace('T', ' ')}`
+
+    orders[user].push({ dateTime: formattedDate, items: orderDetails.items })
+
+    console.log('after adding order orders', orders)
+
+    fs.writeFileSync(ordersFile, JSON.stringify(orders, null, 2))
+
+    res.status(200).send('Order confirmed')
+  } catch (error) {
+    res.status(500).send('Error confirming order')
+  }
+})
+
+app.get('/orders/:user', async (req, res) => {
+  try {
+    const user = req.params.user
+    console.log('getting orders for user: ', user)
+    let orders = {}
+    if (fs.existsSync(ordersFile)) {
+      orders = JSON.parse(fs.readFileSync(ordersFile, 'utf-8'))
+    }
+
+    res.status(200).json(orders[user] || [])
+  } catch (error) {
+    res.status(500).send('Error retrieving orders')
+  }
 })
